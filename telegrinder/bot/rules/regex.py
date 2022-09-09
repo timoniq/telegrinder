4619101aab36d1dab08b27368c35054609b8849b
VodoGamer <54740235+VodoGamer@users.noreply.github.com>
2022-09-09 18:16:03 +0300

    add regex rule (#5)

---
+++ telegrinder/bot/rules/regex.py
@@ -0,0 +1,25 @@
+import re
+from typing import List, Pattern, Union
+
+from .abc import ABCMessageRule, Message
+
+
+class Regex(ABCMessageRule):
+    def __init__(self, regexp: Union[str, List[str], Pattern, List[Pattern]]):
+        if isinstance(regexp, re.Pattern):
+            regexp = [regexp]
+        elif isinstance(regexp, str):
+            regexp = [re.compile(regexp)]
+        else:
+            regexp = [re.compile(regexp) if isinstance(
+                regexp, str) else regexp for regexp in regexp]
+
+        self.regexp = regexp
+
+    async def check(self, message: Message, ctx: dict) -> bool:
+        for regexp in self.regexp:
+            match = re.match(regexp, message.text)
+            if match:
+                ctx.update({"match": match.groups()})
+            return True
+        return False
