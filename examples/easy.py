5bca70a4ec0ec9c85d817d05125eb6108c81a23b
timoniq <tesseradecades@mail.ru>
2022-10-02 19:19:17 +0300

    add example with optional argument

--- examples/easy.py
+++ examples/easy.py
@@ -1,3 +1,6 @@
+import random
+import typing
+
 from telegrinder import Telegrinder, API, Token, Message
 from telegrinder.bot.rules import Text, Markup, FuzzyText
 import logging
@@ -11,7 +14,7 @@ logging.basicConfig(level=logging.INFO)
 async def start(message: Message):
     me = (await api.get_me()).unwrap().first_name
     await message.answer(
-        "Hello, {}! It's {}. How are you today?".format(message.from_.first_name, me),
+        "Hello, {}! It's {}. How are you today?".format(message.from_user.first_name, me),
     )
     m, _ = await bot.on.message.wait_for_message(message.chat.id)
     if m.text.lower() == "fine":
@@ -31,6 +34,17 @@ async def reverse(message: Message, text: str):
         await message.answer(
             text="Wow.. Seems like this is a palindrome.",
         )
+
+
+# In order to make argument optional use this trick:
+#   1) declare patterns ordered by their broadness
+#   2) don't forget to set default value for optional arguments
+@bot.on.message(Markup(["/predict", "/predict <thing>"]))
+async def predict(message: Message, thing: typing.Optional[str] = None):
+    probability_percent = random.randint(0, 100)
+    await message.answer(
+        f"I predict the probability {thing or 'it'} will happen is {probability_percent}%"
+    )
 
 
 @bot.on.message(FuzzyText("hello"))
