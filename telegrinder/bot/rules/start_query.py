14bc44509dd6a20712d9e9a02c1a0dfd4876fd45
Georgy Kritov <77685644+luwqz1@users.noreply.github.com>
2023-03-15 21:12:57 +0300

    new rule StartQuery

---
+++ telegrinder/bot/rules/start_query.py
@@ -0,0 +1,64 @@
+from .abc import ABC, Message, abstractmethod
+from .markup import PatternLike, check_string
+from .text import ABCTextMessageRule
+import typing
+import vbml
+
+
+class ABCQuery(ABC):       
+    @abstractmethod
+    async def check(self, message: Message, query: str, ctx: dict) -> bool:
+        ...
+
+
+class StrQuery(ABCQuery):
+    def __init__(self, value: str) -> None:
+        self.value = value
+    
+    async def check(self, _: Message, query: str, ctx: dict) -> bool:
+        if query == self.value:
+            ctx.update({"start_query": self.value})
+            return True
+        return False
+
+
+class IntQuery(ABCQuery):
+    def __init__(self, value: int) -> None:
+        self.value = value
+    
+    async def check(self, _: Message, query: str, ctx: dict) -> bool:
+        if query.isdigit() and int(query) == self.value:
+            ctx.update({"start_query": self.value})
+            return True
+        return False
+
+
+class MarkupQuery(ABCQuery):
+    def __init__(self, value: PatternLike) -> None:
+        if not isinstance(value, list):
+            value = [value]
+        self.value = [
+            vbml.Pattern(pattern) if isinstance(pattern, str) else pattern
+            for pattern in value
+        ]
+    
+    async def check(self, _: Message, query: str, ctx: dict) -> bool:
+        markup_ctx = {}
+        if check_string(self.value, query, markup_ctx):
+            ctx.update({"start_query": markup_ctx})
+            return True
+        return False
+
+
+class StartQuery(ABCTextMessageRule):
+    def __init__(self, query: typing.Optional[ABCQuery] = None) -> None:
+        self.query = query
+    
+    async def check(self, message: Message, ctx: dict) -> bool:
+        if not message.text.startswith("/start "):
+            return False
+        query = message.text[7:]
+        if self.query is None:
+            ctx.update({"start_query": query})
+            return True
+        return await self.query.check(message, query, ctx)
