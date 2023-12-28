d6527c47f5b028b16ac4e3cb2335a566e699c419
timoniq <arseny.kriuchkov@gmail.com>
2023-12-28 11:13:43 -0300

    fix: generate

--- telegrinder/node/tools/generator.py
+++ telegrinder/node/tools/generator.py
@@ -1,6 +1,6 @@
 import typing
 
-from telegrinder.node.base import Node
+from telegrinder.node.base import ComposeError, Node
 from telegrinder.node.container import ContainerNode
 
 T = typing.TypeVar("T")
@@ -11,14 +11,26 @@ def cast_false_to_none(value: T) -> T | None:
         return None
     return value
 
+def error_on_none(value: T | None) -> T:
+    if value is None:
+        raise ComposeError
+    return value
+
 
 def generate(
     subnodes: tuple[type[Node], ...], 
     func: typing.Callable[..., typing.Any],
-    casts: tuple[typing.Callable, ...] = (cast_false_to_none,),
+    casts: tuple[typing.Callable, ...] = (cast_false_to_none, error_on_none),
 ) -> type[ContainerNode]:
+    
+    @classmethod
     async def compose(cls, **kw) -> typing.Any:
-        args = await super(cls).compose(**kw)
-        return func(args)
+        args = await ContainerNode.compose(**kw)
+        result = func(*args)
+        for cast in casts:
+            result = cast(result)
+        return result
 
-    return ContainerNode.link_nodes(list(subnodes))
+    container = ContainerNode.link_nodes(list(subnodes))
+    container.compose = compose
+    return container
