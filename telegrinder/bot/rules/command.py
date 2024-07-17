36dc0f5dea08fc7bcd92b38c2dbd0f8e8c5641ec
prostomarkeloff <28061158+prostomarkeloff@users.noreply.github.com>
2024-07-17 03:31:44 +0300

    fixed rule

--- telegrinder/bot/rules/command.py
+++ telegrinder/bot/rules/command.py
@@ -1,10 +1,14 @@
 import dataclasses
 import typing
 
+import fntypes
+
 from telegrinder.bot.dispatch.context import Context
 from telegrinder.node.text import Text
+from telegrinder import logger
 
 from .abc import ABCRule
+from telegrinder.node import Source
 
 Validator = typing.Callable[[str], typing.Any | None]
 
@@ -14,11 +18,19 @@ def single_split(s: str, separator: str) -> tuple[str, str]:
     return left, (right[0] if right else "")
 
 
+def cut_mention(text: str) -> tuple[str, str | None]:
+    spl = text.split("@")
+    if len(spl) > 1:
+        return spl[0], spl[1]
+    return spl[0], None
+
+
 @dataclasses.dataclass(frozen=True)
 class Argument:
     name: str
     validators: list[Validator] = dataclasses.field(default_factory=lambda: [])
     optional: bool = dataclasses.field(default=False, kw_only=True)
+
     # NOTE: add optional param `description`
 
     def check(self, data: str) -> typing.Any | None:
@@ -31,18 +43,20 @@ class Argument:
 
 class Command(ABCRule):
     def __init__(
-        self,
-        names: str | typing.Iterable[str],
-        *arguments: Argument,
-        prefixes: tuple[str, ...] = ("/",),
-        separator: str = " ",
-        lazy: bool = False,
+            self,
+            names: str | typing.Iterable[str],
+            *arguments: Argument,
+            prefixes: tuple[str, ...] = ("/",),
+            separator: str = " ",
+            validate_mention: bool = True,
+            lazy: bool = False,
     ) -> None:
         self.names = [names] if isinstance(names, str) else names
         self.arguments = arguments
         self.prefixes = prefixes
         self.separator = separator
         self.lazy = lazy
+        self.validate_mention = validate_mention
 
     def remove_prefix(self, text: str) -> str | None:
         for prefix in self.prefixes:
@@ -51,11 +65,11 @@ class Command(ABCRule):
         return None
 
     def parse_argument(
-        self,
-        arguments: list[Argument],
-        data_s: str,
-        new_s: str,
-        s: str,
+            self,
+            arguments: list[Argument],
+            data_s: str,
+            new_s: str,
+            s: str,
     ) -> dict | None:
         argument = arguments[0]
         data = argument.check(data_s)
@@ -94,20 +108,26 @@ class Command(ABCRule):
 
         return None
 
-    def ignore_mention(self, text: str) -> str:
-        # "cmd@name_bot" always results in "cmd"
-        spl = text.split("@")
-        return spl[0]
-
-    async def check(self, text: Text, ctx: Context) -> bool:
+    async def check(self, text: Text, src: Source, ctx: Context) -> bool:
         text = self.remove_prefix(text)  # type: ignore
         if text is None:
             return False
 
         name, arguments = single_split(text, self.separator)
-        name = self.ignore_mention(name)
+        name, mention = cut_mention(name)
+
         if name not in self.names:
             return False
+
+        if mention is not None and self.validate_mention:
+            me = await src.api.get_me()
+            match me:
+                case fntypes.Ok(res):
+                    if res.username and mention.lower() != res.username.lower():
+                        return False
+                case fntypes.Error(err):
+                    logger.error(err)
+                    return False
 
         if not self.arguments:
             return not arguments
