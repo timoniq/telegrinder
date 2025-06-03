ff9e996ffaf9c4d5a3102338066fd02ffcd2124e
tirch <amerfoe@gmail.com>
2025-06-03 12:29:25 +0300

    added type and __all__

--- telegrinder/node/reply_message.py
+++ telegrinder/node/reply_message.py
@@ -5,5 +5,8 @@ from telegrinder.node import ComposeError, scalar_node
 @scalar_node
 class ReplyMessage:
     @classmethod
-    def compose(cls, message: MessageCute):
+    def compose(cls, message: MessageCute) -> MessageCute:
         return message.reply_to_message.expect(ComposeError("Message doesn't have reply"))
+    
+
+__all__ = ("ReplyMessage")
