712b0d76af52c0155cb6526a4343dcab2746d2fe
luwqz1 <howluwqz1@gmail.com>
2025-07-04 17:52:33 +0300

    uv lock

--- telegrinder/client/__init__.py
+++ telegrinder/client/__init__.py
@@ -1,6 +1,6 @@
-from .abc import ABCClient
-from .aiohttp import AiohttpClient
-from .form_data import MultipartFormProto, encode_form_data
+from telegrinder.client.abc import ABCClient
+from telegrinder.client.aiohttp import AiohttpClient
+from telegrinder.client.form_data import MultipartFormProto, encode_form_data
 
 __all__ = (
     "ABCClient",
