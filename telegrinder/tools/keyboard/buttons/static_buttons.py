78fca8d265177d03b0321389d5131254e7003adb
kesha1225 <samedov03@mail.ru>
2025-07-16 00:13:31 +0300

    fix(static-keyboard): restore payload matching in PayloadModelRule and StaticInlineButton

--- telegrinder/tools/keyboard/buttons/static_buttons.py
+++ telegrinder/tools/keyboard/buttons/static_buttons.py
@@ -9,7 +9,7 @@ from telegrinder.bot.rules.text import Text
 from telegrinder.node.base import Node
 from telegrinder.node.payload import PayloadData
 from telegrinder.tools.keyboard.buttons.base import BaseStaticButton
-from telegrinder.tools.keyboard.buttons.buttons import Button, CallbackData, InlineButton
+from telegrinder.tools.keyboard.buttons.buttons import Button, InlineButton
 
 
 @dataclasses.dataclass
@@ -30,8 +30,6 @@ class StaticButton(StaticButtonMixin, Button, Text):
 
 @dataclasses.dataclass
 class StaticInlineButton(StaticButtonMixin, InlineButton, ABCRule):
-    callback_data_type: type[CallbackData | None]
-
     if not typing.TYPE_CHECKING:
 
         def __init__(self, *args, **kwargs):
@@ -53,6 +51,7 @@ class StaticInlineButton(StaticButtonMixin, InlineButton, ABCRule):
         elif not isinstance(self.callback_data, (bytes, types.NoneType)):
             self.check = PayloadModelRule(
                 type(self.callback_data),
+                payload=self.callback_data,
                 serializer=type(self.callback_data_serializer) if self.callback_data_serializer else None,
             ).check
 
